94c6cf5716297a42fa9a62b7d0ca0d20ab4a26a1
Kinga060406 <kingapolak20@gmail.com>
2024-05-19 19:34:21 +0200

    dodanie wydatków

--- ExpenseSplitApp/View/MainPage.xaml.cs
+++ ExpenseSplitApp/View/MainPage.xaml.cs
@@ -5,41 +5,41 @@ using ExpenseSplitApp.View;
 
 namespace ExpenseSplitApp
 {
-    public partial class MainPage : ContentPage
-    {
-        private ObservableCollection<Group> _groups;
-
-        public MainPage()
+        public partial class MainPage : ContentPage
         {
-            InitializeComponent();
-            LoadGroups();
-        }
+            private ObservableCollection<Group> _groups;
 
-        private async void LoadGroups()
-        {
-            var groups = await App.Database.GetGroupsAsync();
-            _groups = new ObservableCollection<Group>(groups);
-            groupsListView.ItemsSource = _groups;
-        }
-
-        private async void OnAddGroupClicked(object sender, EventArgs e)
-        {
-            var groupName = await DisplayPromptAsync("Nowa Grupa", "Podaj nazwę grupy:");
-            if (!string.IsNullOrWhiteSpace(groupName))
+            public MainPage()
             {
-                var newGroup = new Group { Name = groupName };
-                await App.Database.SaveGroupAsync(newGroup);
-                _groups.Add(newGroup);
+                InitializeComponent();
+                LoadGroups();
             }
-        }
 
-        private async void OnGroupTapped(object sender, ItemTappedEventArgs e)
-        {
-            var group = e.Item as Group;
-            if (group != null)
+            private async void LoadGroups()
             {
-                await Navigation.PushAsync(new ParticipantsPage(group.Id));
+                var groups = await App.Database.GetGroupsAsync();
+                _groups = new ObservableCollection<Group>(groups);
+                groupsListView.ItemsSource = _groups;
+            }
+
+            private async void OnAddGroupClicked(object sender, EventArgs e)
+            {
+                var groupName = await DisplayPromptAsync("Nowa Grupa", "Podaj nazwę grupy:");
+                if (!string.IsNullOrWhiteSpace(groupName))
+                {
+                    var newGroup = new Group { Name = groupName };
+                    await App.Database.SaveGroupAsync(newGroup);
+                    _groups.Add(newGroup);
+                }
+            }
+
+            private async void OnGroupTapped(object sender, ItemTappedEventArgs e)
+            {
+                var group = e.Item as Group;
+                if (group != null)
+                {
+                    await Navigation.PushAsync(new ParticipantsPage(group.Id));
+                }
             }
         }
     }
-}
--- ExpenseSplitApp/View/ParticipantsPage.xaml.cs
+++ ExpenseSplitApp/View/ParticipantsPage.xaml.cs
@@ -5,34 +5,56 @@ using System.Threading.Tasks;
 
 namespace ExpenseSplitApp.View
 {
-    public partial class ParticipantsPage : ContentPage
-    {
-        private ObservableCollection<Participant> _participants;
-        private int _groupId;
-
-        public ParticipantsPage(int groupId)
+        public partial class ParticipantsPage : ContentPage
         {
-            InitializeComponent();
-            _groupId = groupId;
-            LoadParticipants();
-        }
+            private ObservableCollection<Participant> _participants;
+            private ObservableCollection<Expense> _expenses;
+            private int _groupId;
 
-        private async void LoadParticipants()
-        {
-            var participants = await App.Database.GetParticipantsAsync();
-            _participants = new ObservableCollection<Participant>(participants.Where(p => p.GroupId == _groupId));
-            participantsListView.ItemsSource = _participants;
-        }
-
-        private async void OnAddParticipantClicked(object sender, EventArgs e)
-        {
-            var participantName = await DisplayPromptAsync("Nowy Uczestnik", "Podaj nazwê uczestnika:");
-            if (!string.IsNullOrWhiteSpace(participantName))
+            public ParticipantsPage(int groupId)
             {
-                var newParticipant = new Participant { Name = participantName, GroupId = _groupId };
-                await App.Database.SaveParticipantAsync(newParticipant);
-                _participants.Add(newParticipant);
+                InitializeComponent();
+                _groupId = groupId;
+                LoadParticipants();
+                LoadExpenses();
+            }
+
+            private async void LoadParticipants()
+            {
+                var participants = await App.Database.GetParticipantsAsync();
+                _participants = new ObservableCollection<Participant>(participants.Where(p => p.GroupId == _groupId));
+                participantsListView.ItemsSource = _participants;
+            }
+
+            private async void LoadExpenses()
+            {
+                var expenses = await App.Database.GetExpensesAsync();
+                _expenses = new ObservableCollection<Expense>(expenses.Where(e => e.GroupId == _groupId));
+                expensesListView.ItemsSource = _expenses;
+            }
+
+            private async void OnAddParticipantClicked(object sender, EventArgs e)
+            {
+                var participantName = await DisplayPromptAsync("Nowy Uczestnik", "Podaj nazwê uczestnika:");
+                if (!string.IsNullOrWhiteSpace(participantName))
+                {
+                    var newParticipant = new Participant { Name = participantName, GroupId = _groupId };
+                    await App.Database.SaveParticipantAsync(newParticipant);
+                    _participants.Add(newParticipant);
+                }
+            }
+
+            private async void OnAddExpenseClicked(object sender, EventArgs e)
+            {
+                var description = await DisplayPromptAsync("Nowy Wydatek", "Podaj opis wydatku:");
+                var amountString = await DisplayPromptAsync("Nowy Wydatek", "Podaj kwotê:");
+
+                if (!string.IsNullOrWhiteSpace(description) && decimal.TryParse(amountString, out var amount))
+                {
+                    var newExpense = new Expense { Description = description, Amount = amount, GroupId = _groupId };
+                    await App.Database.SaveExpenseAsync(newExpense);
+                    _expenses.Add(newExpense);
+                }
             }
         }
     }
-}
